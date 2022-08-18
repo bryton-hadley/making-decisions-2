db108e009af22c8e89fc73e9c6aca1e112aa0c9a
Bryton Haldey <bryton.hadley@gmail.com>
2022-08-18 16:44:14 -0600

    4th commit

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -84,7 +84,18 @@ var score = 74
 */
 
 //Code Here
-
+if(score <= 60){
+  console.log('F')
+}else if( score <= 69){
+  console.log('D')
+}else if (score <= 79){
+  console.log('C')
+}else if (score <= 89){
+  console.log('B')
+}else if (score <= 99){
+  console.log('A')
+}
+  
 
 ////////// Intermediate Problems //////////
 
